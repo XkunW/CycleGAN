3df463c6a8af639dc0cd0ba17cfe6f396f28353b
XkunW <marshallxkwang@gmail.com>
2021-04-08 05:44:04 -0400

    Change colab to be a base option, recover get_current_visuals

--- CycleGAN/models/cycle_gan_model.py
+++ CycleGAN/models/cycle_gan_model.py
@@ -237,6 +237,14 @@ class CycleGANModel(ABC):
         with torch.no_grad():
             self.forward()
 
+    def get_current_visuals(self):
+        """Return visualization images. train.py will display these images with visdom, and save the images to a HTML"""
+        visual_ret = OrderedDict()
+        for name in self.visual_names:
+            if isinstance(name, str):
+                visual_ret[name] = getattr(self, name)
+        return visual_ret
+
     def get_image_paths(self):
         """ Return image paths that are used to load current data"""
         return self.image_paths
