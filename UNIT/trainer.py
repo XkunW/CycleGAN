794bfc26c936702c6fa7cdebac735c7a9f3ce03a
Sophie <sophietian212@hotmail.com>
2021-04-09 16:25:58 -0400

    minor update to resume function

--- UNIT/trainer.py
+++ UNIT/trainer.py
@@ -166,7 +166,7 @@ class UNIT_Trainer(nn.Module):
         state_dict = torch.load(last_model_name)
         self.gen_a.load_state_dict(state_dict['a'])
         self.gen_b.load_state_dict(state_dict['b'])
-        iterations = int(last_model_name[-11:-3])
+        iterations = int(last_model_name[4:11])
         # Load discriminators
         last_model_name = get_model_list(checkpoint_dir, "dis")
         state_dict = torch.load(last_model_name)
