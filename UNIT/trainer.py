cd604a4e32e7da30b596be6fec3e8c085914409f
TinaBBB <tinashen.tianshu@gmail.com>
2021-04-11 18:12:09 -0400

    Update trainer.py

--- UNIT/trainer.py
+++ UNIT/trainer.py
@@ -168,9 +168,9 @@ class UNIT_Trainer(nn.Module):
         self.gen_a.load_state_dict(state_dict['a'])
         self.gen_b.load_state_dict(state_dict['b'])
         try:
-            iterations = int(last_model_name[4:11])
+            iterations = int(float(last_model_name[4:11]))
         except:
-            iterations = int(last_model_name.split('/')[-1][4:11])
+            iterations = int(float(last_model_name.split('/')[-1][4:11]))
         # Load discriminators
         last_model_name = get_model_list(checkpoint_dir, "dis")
         print('resume from discriminator checkpoint named:', last_model_name)
